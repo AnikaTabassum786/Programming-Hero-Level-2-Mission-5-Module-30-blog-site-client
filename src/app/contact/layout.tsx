c56df0bec765a3487bde90515a2ad8c7e5635512
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-27 20:40:56 +0600

    Mastering Layouts

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,8 @@
+export default function ContactLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+        <p>This is contact layout</p>
+            {children}
+        </>
+    )
+}
