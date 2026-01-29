1bcef3a5e053be2f4152730a57a0b8b8fa472e73
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-29 12:52:51 +0600

    Hierarchy of loading.js, error.js and behavior of loading.js in production build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default async function AboutPage(){
 
     await new Promise((resolve)=>setTimeout(resolve,4000));
 
-    throw new Error("Something went wrong");
+    // throw new Error("Something went wrong");
 
     return(
         <div>
