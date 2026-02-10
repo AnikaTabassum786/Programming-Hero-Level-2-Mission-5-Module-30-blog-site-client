feea180ce32c1f65e6ea82dc8b88e6dd79cdadb6
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-10 09:52:56 +0600

    Making env type safe with proper validation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    API_URL: process.env.API_URL,
+    AUTH_URL: process.env.AUTH_URL,
+    NEXT_PUBLIC_TEST: process.env.NEXT_PUBLIC_TEST,
+  },
 };
 
 export default nextConfig;
