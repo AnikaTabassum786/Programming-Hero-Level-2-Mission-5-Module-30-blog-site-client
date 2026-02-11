7fef38451fa208b427ce22ba7c4ff6c289b6734a
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-11 12:42:09 +0600

    Setting up postman and fetching dummy blog posts

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -12,11 +12,13 @@
 
 
 import { Button } from "@/components/ui/button";
+import { blogService } from "@/services/blog.service";
 import { userService } from "@/services/user.service";
 
 
 export default async function Home() {
- const {data} = await userService.getSession();
+//  const {data} = await userService.getSession();
+ const {data} = await blogService.getBlogPosts();
 
   console.log(data)
   return (
