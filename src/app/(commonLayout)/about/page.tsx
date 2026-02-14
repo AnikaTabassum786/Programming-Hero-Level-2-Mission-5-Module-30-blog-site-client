f6059035fb9efa50a62dbf2cee00e4c147c9462a
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-14 09:55:40 +0600

    Understanding Server Functions and user server

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,14 +1,42 @@
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
-export default async function AboutPage(){
 
-    await new Promise((resolve)=>setTimeout(resolve,4000));
 
-    // throw new Error("Something went wrong");
+// export default async function AboutPage(){
+
+//     await new Promise((resolve)=>setTimeout(resolve,4000));
+
+//     // throw new Error("Something went wrong");
+
+//     return(
+//         <div>
+//         This is About Page
+//         </div>
+//     )
+// }
+
+"use client"
+
+import { getBlogs } from "@/action/blog.action";
+import { useEffect, useState } from "react";
+
+export default function AboutPage(){
+    const [data,setData] = useState();
+    const [error,setError] = useState<{ message: string } | null>(null)
+
+    console.log(data);
+
+    useEffect(()=>{
+        (async()=>{
+            const {data} = await getBlogs();
+            setData(data)
+            setError(error)
+        })();
+    },[])
 
     return(
         <div>
-        This is About Page
+            <p>This is about page component</p>
         </div>
     )
 }
