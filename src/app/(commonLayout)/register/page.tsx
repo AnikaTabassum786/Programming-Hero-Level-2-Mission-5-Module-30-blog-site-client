90f242d46b6360586783c5909e0ca0f04e3df3a3
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-14 12:38:05 +0600

    Getting started with client side form handling

--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -1,16 +1,19 @@
-import { Signup1 } from "@/components/modules/authentication/signup1";
+import { RegisterForm } from "@/components/modules/authentication/signup1";
 
 export default  function Register(){
     return(
         <div>
-        <Signup1 logo={{
+        {/* <Signup1 
+        logo={{
           url: "/",
           src: "/logo.png",
           alt: "Logo",
           title: "My App",
         }}
         buttonText="Login"
-        className=""/>
+        className=""
+        /> */}
+        <RegisterForm></RegisterForm>
         </div>
     )
 }
--- src/components/modules/authentication/signup1.tsx
+++ src/components/modules/authentication/signup1.tsx
@@ -1,87 +1,140 @@
-import { cn } from "@/lib/utils";
+// import { cn } from "@/lib/utils";
+
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+
+// // interface Signup1Props {
+// //   heading?: string;
+// //   logo: {
+// //     url: string;
+// //     src: string;
+// //     alt: string;
+// //     title?: string;
+// //   };
+// //   buttonText?: string;
+// //   googleText?: string;
+// //   signupText?: string;
+// //   signupUrl?: string;
+// //   className?: string;
+// // }
+
+// const Signup1 = () => {
+//   return (
+//     <section className={cn("h-screen bg-muted")}>
+//       <div className="flex h-full items-center justify-center">
+//         {/* Logo */}
+//         <div className="flex flex-col items-center gap-6 lg:justify-start">
+//           {/* <a href={logo.url}>
+           
+//           </a> */}
+//           <div className="flex w-full max-w-sm min-w-sm flex-col items-center gap-y-4 rounded-md border border-muted bg-background px-6 py-8 shadow-md">
+//             {/* {heading && <h1 className="text-xl font-semibold">{heading}</h1>} */}
+//             <Input
+//               type="name"
+//               placeholder="Full Name"
+//               className="text-sm"
+//               required
+//             />
+//             <Input
+//               type="email"
+//               placeholder="Email"
+//               className="text-sm"
+//               required
+//             />
+//             <Input
+//               type="password"
+//               placeholder="Password"
+//               className="text-sm"
+//               required
+//             />
+//             <Input
+//               type="password"
+//               placeholder="Confirm Password"
+//               className="text-sm"
+//               required
+//             />
+//             <Button type="submit" className="w-full">
+//               {/* {buttonText} */}
+//             </Button>
+//           </div>
+//           <div className="flex justify-center gap-1 text-sm text-muted-foreground">
+//             {/* <p>{signupText}</p> */}
+//             <a
+//               // href={signupUrl}
+//               className="font-medium text-primary hover:underline"
+//             >
+//               Login
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export { Signup1 };
+
+
+
+"use client"
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useForm } from "@tanstack/react-form";
 
-interface Signup1Props {
-  heading?: string;
-  logo: {
-    url: string;
-    src: string;
-    alt: string;
-    title?: string;
-  };
-  buttonText?: string;
-  googleText?: string;
-  signupText?: string;
-  signupUrl?: string;
-  className?: string;
-}
 
-const Signup1 = ({
-  heading = "Signup",
-  logo = {
-    url: "https://www.shadcnblocks.com",
-    src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-wordmark.svg",
-    alt: "logo",
-    title: "shadcnblocks.com",
-  },
-  buttonText = "Create Account",
-  signupText = "Already a user?",
-  signupUrl = "https://shadcnblocks.com",
-  className,
-}: Signup1Props) => {
+
+export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
+
+   const form = useForm({
+    defaultValues: {
+      name:" ",
+      email:" ",
+      password:""
+    },
+
+    onSubmit: async({value}) => {
+      console.log(value)
+      console.log("Submitted")
+
+    }
+   })
+  
   return (
-    <section className={cn("h-screen bg-muted", className)}>
-      <div className="flex h-full items-center justify-center">
-        {/* Logo */}
-        <div className="flex flex-col items-center gap-6 lg:justify-start">
-          <a href={logo.url}>
-           
-          </a>
-          <div className="flex w-full max-w-sm min-w-sm flex-col items-center gap-y-4 rounded-md border border-muted bg-background px-6 py-8 shadow-md">
-            {heading && <h1 className="text-xl font-semibold">{heading}</h1>}
-            <Input
-              type="name"
-              placeholder="Full Name"
-              className="text-sm"
-              required
-            />
-            <Input
-              type="email"
-              placeholder="Email"
-              className="text-sm"
-              required
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              className="text-sm"
-              required
-            />
-            <Input
-              type="password"
-              placeholder="Confirm Password"
-              className="text-sm"
-              required
-            />
-            <Button type="submit" className="w-full">
-              {buttonText}
-            </Button>
-          </div>
-          <div className="flex justify-center gap-1 text-sm text-muted-foreground">
-            <p>{signupText}</p>
-            <a
-              href={signupUrl}
-              className="font-medium text-primary hover:underline"
-            >
-              Login
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+    <div className="p-20">
+      <Card {...props}>
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your information below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
 
-export { Signup1 };
+        <form
+        id="register-form"
+        onSubmit={(e)=>{
+          e.preventDefault();
+          form.handleSubmit();
+        }}
+        >
+         
+        </form>
+      
+      </CardContent>
+
+      <CardFooter className="flex justify-end">
+        <Button form="register-form" type="submit">Submit</Button>
+      </CardFooter>
+     
+    </Card>
+    </div>
+  );
+}
