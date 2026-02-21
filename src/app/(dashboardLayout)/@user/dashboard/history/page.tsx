91cb4691b194a070cda0f7e46fcad91d5d8c6bd0
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-21 09:16:50 +0600

    Bootstrapping PaginationControls and handling URLSearchParams in client component

--- src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
@@ -1,20 +1,30 @@
 import HistoryTable from "@/components/modules/user/history/HistoryTable";
+import { PaginationControls } from "@/components/ui/pagination";
 import { blogService } from "@/services/blog.service";
 
-export default async function HistoryPage({searchParams,}:{searchParams:Promise<{page:string}>}){
+export default async function HistoryPage({ searchParams, }: { searchParams: Promise<{ page: string }> }) {
 
-    const {page} = await searchParams;
+    const { page } = await searchParams;
     // console.log(page)
- 
-    const response = await blogService.getBlogPosts({page});
+
+    const response = await blogService.getBlogPosts({ page });
     console.log(response)
 
-    const posts = response.data?.data // [];
+    const posts = response.data?.data || [];
     // console.log(posts)
+
+    const pagination = response.data?.pagination || {
+        limit: 10,
+        page: 1,
+        total: 0,
+        totalPage: 1
+    }
+    // console.log(pagination)
 
     return (
         <>
-          <HistoryTable posts={posts}></HistoryTable>
+            <HistoryTable posts={posts}></HistoryTable>
+            <PaginationControls meta={pagination}></PaginationControls>
         </>
     )
 }
---
+++ src/components/ui/pagination.tsx
@@ -0,0 +1,215 @@
+// import * as React from "react"
+// import {
+//   ChevronLeftIcon,
+//   ChevronRightIcon,
+//   MoreHorizontalIcon,
+// } from "lucide-react"
+
+// import { cn } from "@/lib/utils"
+// import { buttonVariants, type Button } from "@/components/ui/button"
+
+// function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
+//   return (
+//     <nav
+//       role="navigation"
+//       aria-label="pagination"
+//       data-slot="pagination"
+//       className={cn("mx-auto flex w-full justify-center", className)}
+//       {...props}
+//     />
+//   )
+// }
+
+// function PaginationContent({
+//   className,
+//   ...props
+// }: React.ComponentProps<"ul">) {
+//   return (
+//     <ul
+//       data-slot="pagination-content"
+//       className={cn("flex flex-row items-center gap-1", className)}
+//       {...props}
+//     />
+//   )
+// }
+
+// function PaginationItem({ ...props }: React.ComponentProps<"li">) {
+//   return <li data-slot="pagination-item" {...props} />
+// }
+
+// type PaginationLinkProps = {
+//   isActive?: boolean
+// } & Pick<React.ComponentProps<typeof Button>, "size"> &
+//   React.ComponentProps<"a">
+
+// function PaginationLink({
+//   className,
+//   isActive,
+//   size = "icon",
+//   ...props
+// }: PaginationLinkProps) {
+//   return (
+//     <a
+//       aria-current={isActive ? "page" : undefined}
+//       data-slot="pagination-link"
+//       data-active={isActive}
+//       className={cn(
+//         buttonVariants({
+//           variant: isActive ? "outline" : "ghost",
+//           size,
+//         }),
+//         className
+//       )}
+//       {...props}
+//     />
+//   )
+// }
+
+// function PaginationPrevious({
+//   className,
+//   ...props
+// }: React.ComponentProps<typeof PaginationLink>) {
+//   return (
+//     <PaginationLink
+//       aria-label="Go to previous page"
+//       size="default"
+//       className={cn("gap-1 px-2.5 sm:pl-2.5", className)}
+//       {...props}
+//     >
+//       <ChevronLeftIcon />
+//       <span className="hidden sm:block">Previous</span>
+//     </PaginationLink>
+//   )
+// }
+
+// function PaginationNext({
+//   className,
+//   ...props
+// }: React.ComponentProps<typeof PaginationLink>) {
+//   return (
+//     <PaginationLink
+//       aria-label="Go to next page"
+//       size="default"
+//       className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
+//       {...props}
+//     >
+//       <span className="hidden sm:block">Next</span>
+//       <ChevronRightIcon />
+//     </PaginationLink>
+//   )
+// }
+
+// function PaginationEllipsis({
+//   className,
+//   ...props
+// }: React.ComponentProps<"span">) {
+//   return (
+//     <span
+//       aria-hidden
+//       data-slot="pagination-ellipsis"
+//       className={cn("flex size-9 items-center justify-center", className)}
+//       {...props}
+//     >
+//       <MoreHorizontalIcon className="size-4" />
+//       <span className="sr-only">More pages</span>
+//     </span>
+//   )
+// }
+
+// export {
+//   Pagination,
+//   PaginationContent,
+//   PaginationLink,
+//   PaginationItem,
+//   PaginationPrevious,
+//   PaginationNext,
+//   PaginationEllipsis,
+// }
+
+
+"use client"
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
+import { Button } from "./button";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface PaginationControlsProps {
+  meta: {
+    limit: number;
+    page: number;
+    total: number;
+    totalPage: number;
+  }
+}
+
+export function PaginationControls({ meta }: PaginationControlsProps) {
+
+  const { limit, page, total, totalPage } = meta
+  // console.log({limit,page,total,totalPage})
+
+  const searchParams = useSearchParams();
+  const router = useRouter()
+  // console.log(searchParams.get("page"));
+
+  const navigateToPage = (page: number) => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set("page", page.toString())
+    router.push(`?${params.toString()}`)
+  }
+
+
+
+  return (
+    <>
+      <div className="flex items-center justify-between px-2 py-4 border-t mt-4">
+        <div className="text-sm text-muted-foreground">
+
+        </div>
+
+        <div className="flex items-center space-x-2">
+          {/* <Button
+          variant="outline"
+          size="icon"
+         
+        >
+          <ChevronsLeft className="h-4 w-4" />
+        </Button> */}
+
+          <Button
+            variant="outline"
+            size="icon"
+
+          >
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
+
+          <div className="flex items-center gap-1">
+            <span className="text-sm font-medium">
+              1
+            </span>
+          </div>
+
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={() => { navigateToPage(page + 1) }}
+          >
+            <ChevronRight className="h-4 w-4" />
+          </Button>
+
+          {/* <Button
+          variant="outline"
+          size="icon"
+          
+        >
+          <ChevronsRight className="h-4 w-4" />
+        </Button> */}
+        </div>
+      </div>
+    </>
+  )
+}
